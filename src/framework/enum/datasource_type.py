1e705b87ca4c4afd92f550f3a17d67cfec3eb984
Elena Sacramento <elena.sacramento80@gmail.com>
2024-05-02 22:18:39 +0100

    :building_construction: Add new datasource

--- src/framework/enum/datasource_type.py
+++ src/framework/enum/datasource_type.py
@@ -1,12 +1,14 @@
 from enum import Enum
 from typing import List
 
-DATA_FOLDER = '../../data/'
+DATA_FOLDER = '../../data'
 
 
 class DatasourceType(Enum):
 
-    STOP_AND_SEARCH_DYFED_POWYS = ("Dyfed Powys - Stop and Search", f"{DATA_FOLDER}dyfed-powys/")
+    STOP_AND_SEARCH_DYFED_POWYS = ("Dyfed Powys - Stop and Search", f"{DATA_FOLDER}/stop-and-search/dyfed-powys/")
+    STOP_AND_SEARCH_CAMBRIDGESHIRE = ("Cambridgeshire - Stop and Search", f"{DATA_FOLDER}/stop-and-search/cambridgeshire/")
+    UK_ACCIDENTS = ("United Kingdom - Accidents ", f"{DATA_FOLDER}/uk-accidents/")
 
     def __new__(cls, name, path):
         datasource = object.__new__(cls)
@@ -28,3 +30,9 @@ class DatasourceType(Enum):
 
     def is_dyfed_powys(self) -> bool:
         return self == DatasourceType.STOP_AND_SEARCH_DYFED_POWYS
+
+    def is_cambridgeshire(self) -> bool:
+        return self == DatasourceType.STOP_AND_SEARCH_CAMBRIDGESHIRE
+
+    def is_uk_accidents(self) -> bool:
+        return self == DatasourceType.UK_ACCIDENTS
